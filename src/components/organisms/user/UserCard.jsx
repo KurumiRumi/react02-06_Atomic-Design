179188cb7694156157a93e68fe58519c6dff96c4
KurumiRumi <40328870+KurumiRumi@users.noreply.github.com>
2022-07-30 15:05:18 +0900

    32. Organismの作成1

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>11111@aaa.com</dd>
+        <dt>TEL</dt>
+        <dd>000-0000-0000</dd>
+        <dt>会社名</dt>
+        <dd>aaaaa会社</dd>
+        <dt>WEB</dt>
+        <dd>htttttt.com</dd>
+      </dl>
+    </div>
+  );
+};
