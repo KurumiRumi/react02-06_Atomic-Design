29ffa36068590a491ece406ec51f0ff8a59bc3b3
KurumiRumi <40328870+KurumiRumi@users.noreply.github.com>
2022-07-30 17:17:01 +0900

    38. ツラい例を実装してみる

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+import { useLocation } from "react-router-dom";
+
 import { SearchInput } from "../molecules/SearchInput";
 import { UserCard } from "../organisms/user/UserCard";
 
@@ -18,13 +20,16 @@ const users = [...Array(10).keys()].map((val) => {
 });
 
 export const Users = () => {
+  const { state } = useLocation();
+  const isAdmin = state ? state.isAdmin : false;
+
   return (
     <SContainer>
       <h2>ユーザ一覧</h2>
       <SearchInput />
       <SUserArea>
         {users.map((user) => (
-          <UserCard key={user.id} user={user} />
+          <UserCard key={user.id} user={user} isAdmin={isAdmin} />
         ))}
       </SUserArea>
     </SContainer>
