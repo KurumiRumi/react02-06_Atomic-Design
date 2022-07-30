f6dab514d1dceb6347533d66235d675e9d0b2997
KurumiRumi <40328870+KurumiRumi@users.noreply.github.com>
2022-07-30 15:31:14 +0900

    33. Organismの作成2(修正)

--- src/components/molecules/user/UserIconWithName.jsx
+++ src/components/molecules/user/UserIconWithName.jsx
@@ -4,7 +4,7 @@ export const UserIconWithName = (props) => {
   const { image, name } = props;
   return (
     <SContainer>
-      <SImf height={160} width={160} src={image} alt={name} />
+      <SImg height={160} width={160} src={image} alt={name} />
       <SName>{name}</SName>
     </SContainer>
   );
@@ -13,7 +13,7 @@ export const UserIconWithName = (props) => {
 const SContainer = styled.div`
   text-align: center;
 `;
-const SImf = styled.img`
+const SImg = styled.img`
   border-radius: 50%;
 `;
 const SName = styled.p`
